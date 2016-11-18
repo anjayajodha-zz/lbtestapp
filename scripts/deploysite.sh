77ae7b60d3515ef24013c065ade51710bed51826
Anjay Ajodha <anajod@microsoft.com>
2016-11-17 16:49:44 -0800

    Upgraded scripts

---
+++ scripts/deploysite.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#Script runs as root, be careful!
+#Install node.js
+
+curl -sL https://deb.nodesource.com/setup_4.x | bash -
+apt-get -qq install -y nodejs
+
+#Install Git
+apt-get -qq install -y git
+
+#Install MongoDB
+#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+#echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+#apt-get -qq update
+
+#apt-get -qq install -y mongodb-org
+
+#service mongod start
+
+#Clone Repo
+
+git clone https://192.168.102.9/root/demowebapp.git /lbtestapp
+
+cd /lbtestapp
+
+#Install Dependencies
+npm install
+
+#install forever to start node in background
+npm install -g forever
+
+#stop node
+killall nodejs
+
+#Start app
+forever start server.js
